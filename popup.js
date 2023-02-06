a0876b628d66004c26d84c7b0ea0dcc56fcd63bf
noricha-vr <noricha.vr@gmail.com>
2023-02-06 17:32:08 +0800

    get current url then change input value.

--- popup.js
+++ popup.js
@@ -12,10 +12,45 @@ function updateProgress(value) {
 	document.getElementById("progressbar").value = value;
 }
 
+async function copyToClipboard(text) {
+	let result = true;
+	try {
+		navigator.clipboard.writeText(text);
+	} catch (err) {
+		result = false;
+		console.error('Failed to copy text: ', err);
+	}
+	return result;
+}
+
+async function fetchMovieURL(post_url) {
+	const url = window.location.href;
+	const input_data = {
+		url: post_url,
+		lang: window.navigator.language,
+		page_height: '20000',
+		wait_time: 5
+	};
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(input_data)
+	};
+	let response = await fetch('https://web-screen.net/api/url-to-movie/', options);
+	let output_data = await response.json();
+	console.log(output_data);
+	return output_data['url'];
+}
+
 async function main() {
-	let url = await getUrl();
-	let value = 50;
-	updateProgress(value);
-	alert('current utl: ' + url);
+	// let value = 100;
+	// updateProgress(value);
+	let input_url = await getUrl();
+	// let movie_url = await fetchMovieURL(input_url);
+	let movie_url = input_url;
+	document.getElementById('movie_url').value = movie_url;
+	alert('movie_url: ' + movie_url);
 }
 main();
