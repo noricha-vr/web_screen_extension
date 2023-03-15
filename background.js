8a6fa7a4a4f67dcd0c94381b92eac250ab83f009
noricha-vr <noricha.vr@gmail.com>
2023-03-15 14:25:58 +0900

    Fix download error. Add: History

--- background.js
+++ background.js
@@ -4,3 +4,18 @@ chrome.action.onClicked.addListener((tab) => {
 		files: ['content.js']
 	});
 });
+
+// background.js
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.movieUrl && request.domain) {
+		chrome.downloads.download({
+			url: request.movieUrl,
+			filename: request.domain + ".mp4",
+			saveAs: true
+		}, function (downloadId) {
+			sendResponse("Download started");
+		});
+	}
+	return true;
+});
--- popup.js
+++ popup.js
@@ -3,6 +3,10 @@ const progressArea = document.getElementById("progress-area");
 const successArea = document.getElementById("success-area");
 const inputText = document.getElementById('movie_url')
 const copyButton = document.getElementById('copy-button');
+const historyArea = document.getElementById("history-area");
+const convertButton = document.getElementById("convert-button");
+const autoCopyCheckbox = document.getElementById("autoCopy");
+let historyList = [];
 let progressValue = 0;
 
 async function getUrl() {
@@ -48,8 +52,7 @@ async function fetchMovieURL(post_url) {
 	}
 }
 
-const convertButton = document.getElementById("convert-button");
-const autoCopyCheckbox = document.getElementById("autoCopy");
+
 convertButton.addEventListener("click", convertToMovie);
 
 async function convertToMovie() {
@@ -58,29 +61,88 @@ async function convertToMovie() {
 	convertButton.style.display = 'none';
 	let interval = setInterval(updateProgress, 150);
 
-	let input_url = await getUrl();
-	let movie_url = await fetchMovieURL(input_url);
-	inputText.value = movie_url;
+	let inputUrl = await getUrl();
+	let movieUrl = await fetchMovieURL(inputUrl);
+	inputText.value = movieUrl;
 	// copy input text value to clipboard
 	clearInterval(interval);
 	progressBar.value = 0;
 	progressArea.style.display = 'none';
 	successArea.style.display = '';
 	convertButton.style.display = '';
-	inputText.value = movie_url;
+	inputText.value = movieUrl;
+	addHistoryItem(inputUrl, movieUrl);
 	if (autoCopyCheckbox.checked) {
-		clickCopy();
+		navigator.clipboard.writeText(movieUrl);
 	}
+	let historyItem = {
+		inputUrl: inputUrl,
+		movieUrl: movieUrl,
+		createdDate: new Date()
+	};
+	historyList.push(historyItem);
+	chrome.storage.local.set({ "historyList": historyList });
+}
+
+copyButton.addEventListener('click', () => {
+	navigator.clipboard.writeText(inputText.value);
+});
+
+function addHistoryItem(inputUrl, movieUrl) {
+	const itemDiv = document.createElement("div");
+	itemDiv.classList.add("history-item");
+
+	const copyButton = document.createElement("button");
+	copyButton.textContent = "Copy";
+	copyButton.addEventListener("click", () => {
+		navigator.clipboard.writeText(movieUrl);
+	});
+	itemDiv.appendChild(copyButton);
+
+	const downloadButton = document.createElement("a");
+	downloadButton.textContent = "DL";
+	downloadButton.href = movieUrl;
+	downloadButton.download = inputUrl.split('/')[2] + ".mp4";
+	downloadButton.classList = "btn btn-primary";
+	downloadButton.addEventListener("click", () => {
+		chrome.runtime.sendMessage(
+			{ movieUrl: movieUrl, domain: inputUrl.split('/')[2] },
+			function (response) {
+				console.log(response);
+			}
+		);
+	})
+
+	itemDiv.appendChild(downloadButton);
+
+
+	const link = document.createElement("a");
+	link.textContent = inputUrl.split('/')[2];
+	link.href = movieUrl;
+	link.target = "_blank";
+	itemDiv.appendChild(link);
+
+	const deleteButton = document.createElement("button");
+	deleteButton.textContent = "Delete";
+	deleteButton.addEventListener("click", () => {
+		itemDiv.remove();
+	});
+	itemDiv.appendChild(deleteButton);
+
+	historyArea.appendChild(itemDiv);
 }
 
 
-function clickCopy() {
-	inputText.select();
-	inputText.setSelectionRange(0, 99999);
-	document.execCommand("copy");
-}
-copyButton.addEventListener('click', clickCopy);
-
-window.load = async () => {
+window.onload = async () => {
+	chrome.storage.local.get("historyList", function (result) {
+		if (result.historyList) {
+			historyList = result.historyList;
+			historyList.forEach((item) => {
+				addHistoryItem(item.inputUrl, item.movieUrl);
+			});
+		} else {
+			historyList = [];
+		}
+	});
 }
 
